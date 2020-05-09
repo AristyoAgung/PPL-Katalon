cf5b177ac059fb62c626a10515ab1a8ff707b6a8
AristyoAgung <54440579+AristyoAgung@users.noreply.github.com>
2020-05-09 07:12:22 +0700

    Initial commit

---
+++ Object Repository/Page_Tokobuah Admin - Products/div_NamePricePhotoDescriptionExpired Date.rs
@@ -0,0 +1,113 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_NamePricePhotoDescriptionExpired Date</name>
+   <tag></tag>
+   <elementGuidId>ed7fc0a6-0da2-494d-b7f3-cf5347aca6f4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='content-wrapper']/div/div/div[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>card-body</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+
+						
+							
+								Name*
+								
+								
+																	
+							
+
+							
+								Price*
+								
+								
+																	
+							
+
+
+							
+								Photo
+								
+								
+																	
+							
+
+							
+								Description*
+								
+								
+																	
+							
+
+							
+								Expired Date*
+								
+								
+																	
+							
+
+							
+						
+
+					</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;content-wrapper&quot;)/div[@class=&quot;container-fluid&quot;]/div[@class=&quot;card mb-3&quot;]/div[@class=&quot;card-body&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='content-wrapper']/div/div/div[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Back'])[1]/following::div[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Add'])[1]/following::div[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/Uji Coba Input dengan Data Tes.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Uji Coba Input dengan Data Tes</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8dcf93a8-c062-4c74-b9c9-55dd4382702a</testSuiteGuid>
+   <testCaseLink>
+      <guid>04675ebc-2e29-4f10-85e1-2e1a3d14466c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case Input Barang - Variabel</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>0ce1fba3-c1ed-4fb4-bfa4-6cee15a62421</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Input Data</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>0ce1fba3-c1ed-4fb4-bfa4-6cee15a62421</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Name</value>
+         <variableId>a7b483f2-d769-4e83-bfd8-30ca14656321</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>0ce1fba3-c1ed-4fb4-bfa4-6cee15a62421</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Price</value>
+         <variableId>dac1673e-bc86-46fa-a537-31988b20425b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>0ce1fba3-c1ed-4fb4-bfa4-6cee15a62421</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Description</value>
+         <variableId>2126d46d-078d-4dc9-a5f2-82e13bb79a14</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f96e0670-6ada-49f2-9953-fdfbde5f78a4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Validasi List Produk</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>799a4b68-e4d5-4256-80cf-e6322c9f4420</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Input Data</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>799a4b68-e4d5-4256-80cf-e6322c9f4420</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Name</value>
+         <variableId>0c846a8f-5b3e-4130-a7f7-ae76f601b85a</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
